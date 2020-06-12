d3e6227a7d3b553d4e66b92278c475625a035c70
Caleb Redd <caleb.redd@yahoo.com>
2020-06-11 21:48:39 -0700

    Optimized....

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,18 @@
 Input: an integer
 Returns: an integer
 '''
+cache={}
+cache[2]=2
+cache[1]=1
+cache[0]=1
 def eating_cookies(n):
     # Your code here
-    if( n == 1 or n == 0 ):
-        return 1
-    elif ( n==2 ):
-        return 2
+    if n in cache:
+        return cache[n]
     else:
+        cache[n-3]=eating_cookies(n-3)
+        cache[n-2]=eating_cookies(n-2)
+        cache[n-1]=eating_cookies(n-1)
         return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1) 
 
 if __name__ == "__main__":
